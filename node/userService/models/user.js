14e5ea96b9098467adb38fa25b0f1b5757d6e1df
manuj55 <manuj6885@gmail.com>
2025-01-17 12:25:20 +0100

    feat(user): added user model

---
+++ node/userService/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//Define the user schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 6
+    }
+});
+
+
+
+//Pre save hook to hash the password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+// Method to compare provided password with the stored hashed password
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+//create a user model 
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
