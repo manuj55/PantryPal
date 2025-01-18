365286463ca91cbb9dcf7d58c4e29812ad7228d6
Pramukh Prakash <pramukhp35@gmail.com>
2025-01-18 23:22:23 +0100

    added swagger and swagerConfigFile

--- java/productService/src/main/java/com/pramukh/productservice/Controller/ProductController.java
+++ java/productService/src/main/java/com/pramukh/productservice/Controller/ProductController.java
@@ -2,8 +2,9 @@ package com.pramukh.productservice.Controller;
 
 import com.pramukh.productservice.DTO.ProductRequestDto;
 import com.pramukh.productservice.DTO.ProductResponseDto;
-import com.pramukh.productservice.Model.ProductEntity;
 import com.pramukh.productservice.Service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +17,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Product API's")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @PostMapping(value = "/products", consumes = "multipart/form-data")
+    @Operation(summary = "Add products")
     public ResponseEntity<String> getProducts(@RequestPart("product") List<ProductRequestDto> productRequestDto, @RequestPart("image") List<MultipartFile> file) throws IOException {
         productService.addProducts(productRequestDto, file);
         return new ResponseEntity<>("Product added successfully", HttpStatus.OK);
     }
 
     @GetMapping("/products")
+    @Operation(summary = "Get all products")
     public ResponseEntity<List<ProductResponseDto>> getProducts() {
         List<ProductResponseDto> products = productService.getProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
@@ -35,12 +39,14 @@ public class ProductController {
     }
 
     @GetMapping("/products/category/{category}")
+    @Operation(summary = "Get products by category")
     public ResponseEntity<List<ProductResponseDto>> getProductsByCategory(@PathVariable String category) {
         List<ProductResponseDto> products = productService.getProductsByCategory(category);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/products/{name}")
+    @Operation(summary = "Get products by name")
     public ResponseEntity<List<ProductResponseDto>> getProductsByName(@PathVariable String name) {
         List<ProductResponseDto> products = productService.getProductsByName(name);
         return new ResponseEntity<>(products, HttpStatus.OK);
