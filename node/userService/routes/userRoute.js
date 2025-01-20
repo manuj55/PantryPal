ed886a07134cc4da2c00f818e9922b9a38431aed
manuj55 <manuj6885@gmail.com>
2025-01-20 11:28:46 +0100

    docs(swagger): added swagger documentation for user service

--- node/userService/routes/userRoute.js
+++ node/userService/routes/userRoute.js
@@ -2,6 +2,45 @@ const express = require("express");
 const User = require("../models/user")
 const router = express.Router();
 
+
+/**
+         * POST Methods
+         * @openapi
+         * '/api/user/register':
+         *  post:
+         *     tags:
+         *     - User Controller
+         *     summary: Create a user
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - name
+         *               - email
+         *               - password
+         *             properties:
+         *               name:
+         *                 type: string
+         *                 default: johndoe
+         *               email:
+         *                 type: string
+         *                 default: johndoe@mail.com
+         *               password:
+         *                 type: string
+         *                 default: johnDoe20!@
+         *     responses:
+         *       201:
+         *         description: Created
+         *       409:
+         *         description: Conflict
+         *       404:
+         *         description: Not Found
+         *       500:
+         *         description: Server Error
+         */
 //create user 
 router.post("/", async (req, res) => {
     const { name, email, password } = req.body;
@@ -33,6 +72,20 @@ router.post("/", async (req, res) => {
 })
 
 
+/**
+ * GET User
+ * @openapi
+ * '/api/users':
+ *   get:
+ *     tags:
+ *       - User Controller
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 //get user 
 router.get("/", async (req, res) => {
     try {
@@ -46,6 +99,27 @@ router.get("/", async (req, res) => {
     }
 })
 
+/**
+ * GET user by id
+ * @openapi
+ * '/api/users/{id}':
+ *   get:
+ *     tags:
+ *       - User Controller
+ *     summary: Get user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Unique identifier of the user
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 
 //get user by id
 router.get("/:id", async (req, res) => {
