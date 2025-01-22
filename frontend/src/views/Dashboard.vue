23f59b762c176720e7d3ea1a42b7985f0791c791
Yathish <yathish2012@gmail.com>
2025-01-23 00:24:06 +0100

    feat(vue): add vue boiler template with reusable components

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="container-fluid py-4">
+
+    <div class="row ">
+      <div
+        v-for="(product, index) in products"
+        :key="product.name"
+        class="col-xl-4 col-md-6 mb-4"
+      >
+
+        <ProductCard
+          :product="product"
+          @update-quantity="updateQuantity(index, $event)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductCard from "./components/ProductCard.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      products: [
+        {
+          photo: require('@/assets/img/dashboard/big-mac.png'),
+          name: "Big Mac",
+          price: 5.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/mc-chicken.png'),
+          name: "Mc Chicken",
+          price: 4.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/double-cb.png'),
+          name: "Double Cheese Burger",
+          price: 2.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/fries.png'),
+          name: "Fries",
+          price: 2.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/nuggets.png'),
+          name: "Mc Nuggets",
+          price: 3.49,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/salad.png'),
+          name: "Salad",
+          price: 2.79,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/cola.png'),
+          name: "Coke",
+          price: 1.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/lipton.png'),
+          name: "Ice Tea",
+          price: 1.99,
+          quantity: 1,
+        },
+        {
+          photo: require('@/assets/img/dashboard/water.png'),
+          name: "Water",
+          price: 1.49,
+          quantity: 1,
+        },
+      ],
+    };
+  },
+  methods: {
+   
+    updateQuantity(index, newQuantity) {
+      console.log(`Updating product at index ${index} with new quantity: ${newQuantity}`);
+      if (newQuantity > 0) {
+
+        this.products[index].quantity = newQuantity;
+      }
+    },
+  },
+};
+</script>
