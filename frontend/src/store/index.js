dfeaf58375510b281caf985eb94df5a659f17d21
QuickStack-Library <mjgowda27g@gmail.com>
2025-02-03 20:41:36 +0530

    fix(merge conflict)

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -241,7 +241,7 @@ export default createStore({
     async fetchOrders({ commit }) {
       try {
         const token = localStorage.getItem("authToken");  
-        if (!token || this.state.userId ) {
+        if (!token ) {
           console.error("No auth token found.");
           return;
         }
